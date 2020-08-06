4cc1f6e21a346f03b84cec145355283abf8b3ec2
Kelli Budd <buddkelli1@gmail.com>
2020-08-05 20:47:00 -0500

    Add source link

--- removefromll.py
+++ removefromll.py
@@ -6,6 +6,8 @@
 #
 def removeKFromList(l, k):
     """
+    Source: https://app.codesignal.com/interview-practice/task/gX7NXPBrYThXZuanm
+
     Given a singly linked list of integers l and an integer k,
     remove all elements from list l that have a value equal to k.
 
